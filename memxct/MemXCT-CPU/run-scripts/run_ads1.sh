f0ab86db61be8883bf895f76378b55273a4fe0b9
Brock Davis <btdavis@login4.stampede2.tacc.utexas.edu>
2020-10-07 21:54:24 -0500

    Modified run scripts so that weak scaling is possible

--- memxct/MemXCT-CPU/run-scripts/run_ads1.sh
+++ memxct/MemXCT-CPU/run-scripts/run_ads1.sh
@@ -2,9 +2,32 @@
 
 mkdir -p o-files
 
-for CPUS in $@
-do
-	PROCS=`expr $CPUS \* 16`
-	echo "sbatch -N $CPUS -n $PROCS ads1.slurm"
-	sbatch -N $CPUS -n $PROCS ads1.slurm
-done
+CORES=68
+BASE_THETA=360
+BASE_RHO=256
+
+if [[ $# == 2 ]]; then
+
+	NODES=$1
+
+	# Export processes to run script
+	PROCS=`expr $NODES \* $CORES`
+
+	FACTOR=$2
+
+	NTHETA=`expr $BASE_THETA \* $FACTOR`
+	NRHO=`expr $BASE_RHO \* $FACTOR`
+
+	# export env and batch
+	echo "NUMTHE=$NTHETA"
+	export NUMTHE=$NTHETA
+
+	echo "NUMRHO=$NRHO"
+	export NUMRHO=$NRHO
+
+	echo "sbatch -N $NODES -n $PROCS ads1.slurm"
+	sbatch -N $NODES -n $PROCS ads1.slurm
+else
+	echo "Please put 2 arguments"
+	echo "./run_ads.sh <n_CPUS> <SCALE_FACTOR>"
+fi
--- memxct/MemXCT-CPU/run-scripts/run_ads4.sh
+++ memxct/MemXCT-CPU/run-scripts/run_ads4.sh
@@ -2,9 +2,33 @@
 
 mkdir -p o-files
 
-for CPUS in $@
-do
-	PROCS=`expr $CPUS \* 16`
-	echo "sbatch -N $CPUS -n $PROCS ads4.slurm"
-	sbatch -N $CPUS -n $PROCS ads4.slurm
-done
+CORES=68
+BASE_THETA=2400
+BASE_RHO=2048
+
+
+if [[ $# == 2 ]]; then
+
+	NODES=$1
+
+	# Export processes to run script
+	PROCS=`expr $NODES \* $CORES`
+
+	FACTOR=$2
+
+	NTHETA=`expr $BASE_THETA \* $FACTOR`
+	NRHO=`expr $BASE_RHO \* $FACTOR`
+
+	# export env and batch
+	echo "NUMTHE=$NTHETA"
+	export NUMTHE=$NTHETA
+
+	echo "NUMRHO=$NRHO"
+	export NUMRHO=$NRHO
+
+	echo "sbatch -N $NODES -n $PROCS ads4.slurm"
+	sbatch -N $NODES -n $PROCS ads4.slurm
+else
+	echo "Please put 2 arguments"
+	echo "./run_ads.sh <n_CPUS> <SCALE_FACTOR>"
+fi

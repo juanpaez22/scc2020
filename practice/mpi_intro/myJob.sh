2869dd3f565ffc4724eabcb884acd70a23ece61e
Nicholas Aufiero <ccuser@ip-0A00000C.w1nklnscc4iubckbypucuu3mhc.jx.internal.cloudapp.net>
2020-10-21 00:02:41 +0000

    new job script for testing mpi stacks

--- practice/mpi_intro/myJob.sh
+++ practice/mpi_intro/myJob.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 #SBATCH -J myjob           # Job name
 #SBATCH -o myjob.o%j       # Name of output file
-#SBATCH -p test1          # Queue (partition) name
+#SBATCH -p execute           # Queue (partition) name
 #SBATCH -N 2               # Total # of nodes 
-#SBATCH -n 16              # Total # of mpi tasks
+#SBATCH -n 32              # Total # of mpi tasks
 #SBATCH -t 00:30:00        # Run time (hh:mm:ss)
-#SBATCH -A A-ccsc          # Allocation name (req'd if you have more than 1)
+##SBATCH -A A-ccsc          # Allocation name (req'd if you have more than 1)
 
-ml
+date
+module load gcc-9.2.0
+module load mpi/impi_2018.4.274
+#module load mpi/impi_2019.8.254
+#module load mpi/mvapich2
 
 #set -x                     # Echo commands, use "set echo" with csh
-ibrun ./mpiex1big  # Run the MPI executable named <executable name>
+mpirun -np 32 ./impi_2018_hello
+#mpirun -np 32 ./impi_2019_hello
+#mpirun -np 32 ./mvapich2_hello 

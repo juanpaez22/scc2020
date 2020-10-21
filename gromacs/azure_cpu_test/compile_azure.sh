27b7de636bb1ae8097496d2d2e58f6301a73c59b
Nicholas Aufiero <ccuser@ip-0A000014.w1nklnscc4iubckbypucuu3mhc.jx.internal.cloudapp.net>
2020-10-21 06:50:04 +0000

    updates

--- gromacs/azure_cpu_test/compile_azure.sh
+++ gromacs/azure_cpu_test/compile_azure.sh
@@ -1,4 +1,6 @@
-# Install CMake
+#Install CMake
+module load gcc-9.2.0
+module load mpi/impi
 wget https://cmake.org/files/v3.12/cmake-3.12.3.tar.gz
 tar -zxvf cmake-3.12.3.tar.gz
 cd cmake-3.12.3
@@ -12,6 +14,6 @@ cd ..
 wget http://ftp.gromacs.org/pub/gromacs/gromacs-2020.3.tar.gz
 tar -xf gromacs-2020.3.tar.gz
 cd gromacs-2020.3 && mkdir build && cd build
-cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DGMX_GPU=off -DGMX_MPI=on -DGMX_SIMD=AVX2_256 -DGMX_BUILD_UNITTESTS=ON -DGMX_BUILD_OWN_FFTW=ON
+cmake .. -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DGMX_GPU=off -DGMX_MPI=on -DGMX_SIMD=AVX_512 -DGMX_BUILD_UNITTESTS=ON -DGMX_BUILD_OWN_FFTW=ON
 make -j10
-export PATH=/path/to/bin:$PATH
+# REMEMBER TO EXPORT TO PATH
--- gromacs/azure_cpu_test/readme.txt
+++ gromacs/azure_cpu_test/readme.txt
@@ -1,4 +1,6 @@
 The scripts here serve to compile and run gromacs on CycleCloud with Slurm (WIP).
 Notes
-- For some reason, it is RIDICULOUSLY slow. adh_cubic won't finish, and when I kill it, it clocks 0.041 ns/day (61 on Frontera).
-- Compiled with AVX_512 architecture. Is this correct?
+- Compile with AVX_512 architecture
+- htop useful to analyze: sudo yum install -y epel-release, sudo yum install -y htop
+- best result on single node with no GPU (used wrong arch): 7.6 ns/day (was 61 on Frontera-- improve!)
+- to try: multiple nodes, AVX_512 arch, GPU install
